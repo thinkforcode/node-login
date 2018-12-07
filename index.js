3718d07f018faf450cacc7fe4609ae9ea72dc109
manmohan-remedo <manmohan.gupta@remedoapp.com>
2018-12-07 15:04:33 +0530

    adding controller folder

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
     const express = require("express");
     const mongoose = require("mongoose");
     const ejs = require("ejs");
-
+    const bodyParser = require("body-parser");
 
     const port = process.env.PORT || 8000;
     const app = express();
 
+   // setting middleware here
+    app.set('view engine','ejs');
+    app.use(express.static('./public'));
     // listening server here on the port 8000
 
     app.listen(port,(req,res)=>{
